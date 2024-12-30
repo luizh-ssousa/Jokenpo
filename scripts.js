b342fdaaffebb40d23300a36a9c58b45d9af48bf
luizh-ssousa <luizh.ssousa@gmail.com>
2024-12-29 23:28:25 -0300

    add calculo random js

--- scripts.js
+++ scripts.js
@@ -2,4 +2,13 @@
 
 const playHuman = (humanChoice) => {
     console.log(humanChoice)
+
+    console.log(playAI())
+}
+
+const playAI = () => {
+    const choices = ['rock', 'paper', 'scissors']
+    const randomNumber = Math.random() * 3
+
+    return choices [1]
 }
